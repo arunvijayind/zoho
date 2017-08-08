7b132441b58f6fb8e77541722e8470d6018d8dac
arunvijayind <arunvijayind@gmail.com>
2017-08-08 19:23:09 +0530

    Create Knapsack.java

---
+++ Dynamic Programming/Knapsack.java
@@ -0,0 +1,23 @@
+package zoho;
+
+public class Knapsack {
+		public static  void main(String args[]) {
+			
+			int val[]={60,100,120};
+			int weight[]={10,20,30};
+			int capacity=50;
+			
+			System.out.println(knapsack(val,weight,capacity));
+			
+			
+			
+		}
+
+		private static int knapsack(int[] val, int[] weight, int capacity) {
+			
+			
+			
+			
+			return 0;
+		}
+}
